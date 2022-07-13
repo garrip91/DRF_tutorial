a9e9e6d50373ba7314d92780abebf31eaebc5761
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-07-13 21:27:48 +0300

    Update views.py

--- src/quickstart/views.py
+++ src/quickstart/views.py
@@ -10,3 +10,15 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     Конечная точка API, которая позволяет пользователям просматривать или редактировать
     """
+
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    Конечная точка API, которая позволяет просматривать или редактировать группы
+    """
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
