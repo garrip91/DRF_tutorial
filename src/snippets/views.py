816fc9a47a49e320ba3274b4264039f772828e1a
garrip91 <garrip91@gmail.com>
2022-07-14 13:24:19 +0300

    Update views.py

--- src/snippets/views.py
+++ src/snippets/views.py
@@ -9,3 +9,46 @@ from snippets.serializers import SnippetSerializer
 
 
 # Create your views here:
+@csrf_exempt
+def snippet_list(request):
+    """
+    Перечисляем все сниппеты в случае GET-запроса и создаём новый в случае POST-запроса
+    """
+
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Получение, обновление или удаление фрагмента кода
+    """
+
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser.parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
