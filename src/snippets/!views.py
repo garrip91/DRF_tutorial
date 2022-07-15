802644b218da3b131af042213cb68ff40907a2d7
garrip91 <garrip91@gmail.com>
2022-07-15 11:17:13 +0300

    Summary

--- src/snippets/!views.py
+++ src/snippets/!views.py
@@ -1,35 +1,15 @@
 from django.shortcuts import render
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
-from rest_framework import renderers, generics, permissions
-from .permissions import IsOwnerOrReadOnly
-from django.contrib.auth.models import User
-
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics, permissions
+
+from django.contrib.auth.models import User
+
+from .permissions import IsOwnerOrReadOnly
 
 
 # Create your views here:
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format),
-    })
-
-
-class SnippetHighlight(generics.GenericAPIView):
-
-    queryset = Snippet.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer]
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
-
-
 class SnippetList(generics.ListCreateAPIView):
 
     queryset = Snippet.objects.all()
@@ -38,7 +18,7 @@ class SnippetList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
