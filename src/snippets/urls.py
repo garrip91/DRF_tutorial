b60ef656ff71e42f35ff761b4ff3e56c1c347e05
garrip91 <garrip91@gmail.com>
2022-07-15 14:53:44 +0300

    Update urls.py

--- src/snippets/urls.py
+++ src/snippets/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import renderers
 
-from snippets.views import SnippetHighlight, SnippetList, SnippetDetail, UserList, UserDetail, SnippetViewSet, UserViewSet
+from snippets.views import SnippetViewSet, UserViewSet
 
 from rest_framework.routers import DefaultRouter
 
